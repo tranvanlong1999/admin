c13612b915e389dfd59efc73e9675b3068d14064
= <=>
2021-04-17 11:20:03 +0700

    first commit

---
+++ ADMINDOAN/src/main/java/com/doan/admindonghohanquoc/AdmindonghohanquocApplication.java
@@ -0,0 +1,13 @@
+package com.doan.admindonghohanquoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmindonghohanquocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdmindonghohanquocApplication.class, args);
+    }
+
+}
---
+++ ADMINDOAN/src/test/java/com/doan/admindonghohanquoc/AdmindonghohanquocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doan.admindonghohanquoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmindonghohanquocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
